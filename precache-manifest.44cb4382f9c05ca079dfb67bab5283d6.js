74d48f34f6895238432adef51f31e23d86552f06
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-09-09 22:43:41 +0300

    Updates

---
+++ precache-manifest.44cb4382f9c05ca079dfb67bab5283d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4e2b4ee65b6ebc81fcbf2864993001",
+    "url": "/Redux-Api/redapi/index.html"
+  },
+  {
+    "revision": "41667d3adbf7e866d8b0",
+    "url": "/Redux-Api/redapi/static/css/main.c97eab24.chunk.css"
+  },
+  {
+    "revision": "477a2a13ce153623c850",
+    "url": "/Redux-Api/redapi/static/js/2.24f12c46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-Api/redapi/static/js/2.24f12c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41667d3adbf7e866d8b0",
+    "url": "/Redux-Api/redapi/static/js/main.fe70768d.chunk.js"
+  },
+  {
+    "revision": "f39468e32575151980b6",
+    "url": "/Redux-Api/redapi/static/js/runtime-main.e8389822.js"
+  }
+]);
